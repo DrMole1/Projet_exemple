4ac7291698f9801fd9ccc2dce24ac82f3d11ffd4
LAPTOP-PDEB0FD9\basti <bastienprob@gmail.com>
2018-01-23 10:44:40 +0100

    Modification du pendu en modulaire !

--- Pendu.pas
+++ Pendu.pas
@@ -127,7 +127,7 @@
 
 
 }
-
+{
 PROGRAM Pendu;
 // BUT : Le joueur 1 saisit un mot et ne joue plus. Le joueur 2 propose ensuite une lettre par une lettre jusqu'à deviner le mot. Au bout de 15 fautes, il perd (un pendu se dessine progressivement). 
 // ENTREES : Le joueur 1 saisit un mot (chaine de caractères) et le joueur 2 saisit des caractères.
@@ -257,4 +257,209 @@ BEGIN
 
 	readln;
 END.
-	
+}
+///////////////////////////////////
+// Utilisation écriture modulaire//
+///////////////////////////////////
+
+
+PROGRAM Pendu;
+// BUT : Le joueur 1 saisit un mot et ne joue plus. Le joueur 2 propose ensuite une lettre par une lettre jusqu'à deviner le mot. Au bout de 15 fautes, il perd (un pendu se dessine progressivement). 
+// ENTREES : Le joueur 1 saisit un mot (chaine de caractères) et le joueur 2 saisit des caractères.
+// SORTIES : Le progrmme renvoie des booléens en cas d'erreurs ou de victoire et renvoie la chaîne de caractères (dont les caractères non trouvés représentent des cunderscores).
+// PRINCIPE : On mesure la longueur du mot à deviner. On présentera à chaque tour un mot masqué constitué d'autant d'underscores que de lettres ainsi qu'un début de pendu en cas de fautes. Les lettres 
+// précédement trouvées seront affichées. On utilisera pour cela des extractions dans une nouvelle boucle incrémentée dans la première. L'algo se finit si le joueur 2 devine le mot ou s'il commet 15 fautes.
+// NOTE : On utilisera l'écriture modulaire
+
+USES crt;
+
+CONST
+
+	FAUTEMAX = 15;
+
+procedure Bienvenue;     //procédure de Bienvenue, pour expliquer à l'utilisateur pourquoi il est ici
+	begin
+		writeln ('Bienvenue au jeu du pendu !');
+		writeln ('Le joueur saisit un mot, puis le joueur 2 devra le trouver en proposant des lettres. A 15 fautes, il perd !');
+	END;
+
+
+
+function saisieMotAdeviner : string;     //fonction servant au joueur 1 à donner le mot à deviner
+	VAR
+		mot : string;      //déclaration de variable locale intermédiaire (je ne sais pas si c'est nécessaire)
+	begin
+		repeat
+			writeln ('Joueur 1 : veuillez saisir le mot à deviner.');
+			readln (mot);
+		until Length(mot)>0;
+		saisieMotAdeviner := mot;
+	end;
+
+
+
+function longueur(mot_Adeviner : string) : integer;    
+	var
+		check : boolean;
+		cpt : integer;
+	begin
+		check := FALSE;
+		cpt := 1;
+		//lgchaine := Length (mot_Adeviner);
+		repeat
+			check := (Copy (mot_Adeviner,cpt,1) = '');
+			cpt := cpt + 1;
+		until check = TRUE;
+		longueur := cpt-2;
+	end;
+
+
+
+function setMotMasque(lgchaine : integer) : string;
+	VAR
+		mot2 : string;
+		cpt : integer;
+	begin
+		FOR cpt:= 1 TO lgchaine DO
+			begin
+			mot2 := (mot2) + '?';
+			end;
+		setMotMasque := mot2;
+	end;
+
+
+
+procedure resultat(faute : smallint);  //renseigner le joueur 2 de son résultat
+	begin
+		//résultat
+		if (faute = FAUTEMAX) then
+			writeln ('Vous avez perdu !')
+		else
+			writeln ('Vous avez gagne !');
+	end;
+
+
+
+function cptFaute(erreur : boolean; VAR faute : smallint) : integer;    //on met le paramètre (faute) par passage par adresse, car nous le réutilisons !!!
+	begin
+		if (erreur = TRUE) then
+			begin
+			faute := faute + 1;
+			end;
+		cptFaute := faute;
+	end;
+
+
+procedure infoFaute(faute : smallint);
+	begin
+		writeln ('Vous etes à ', faute, ' fautes !');
+	end;
+
+
+
+procedure dessinPendu(faute : smallint);
+	begin
+		if (faute >= 8) then
+			writeln ('________');  //écrire à la ligne
+		if (faute >= 7) then
+			write (' !');  //écrire à la ligne
+		if (faute >= 9) then
+			writeln ('    !');  
+		if (faute >= 6) then
+			write (' !');  //écrire à la ligne
+		if (faute >= 10) then
+			writeln ('    O');  
+		if (faute >= 5) then
+			write (' !');  //écrire à la ligne
+		if (faute >= 11) then
+			write ('   /');  
+		if (faute >= 12) then
+			write ('!');  
+		if (faute >= 13) then
+			writeln ('\');  
+		if (faute >= 4) then
+			write (' !');  //écrire à la ligne
+		if (faute >= 14) then
+			write ('   /');  
+		if (faute >= 15) then
+			writeln (' \');  
+		if (faute >= 1) then
+			write ('_');  //écrire à la ligne
+		if (faute >= 2) then
+			write ('!');  
+		if (faute >= 3) then
+			write ('_');  
+	end;
+
+
+VAR
+
+	mot_Masque, mot_Adeviner : string;
+	car_Rech : char;
+	cpt, lgchaine : integer;
+	faute : smallint;
+	erreur : boolean;
+
+BEGIN //bloc principal MAIN
+
+	clrscr;
+
+	//on initialise nos varibales
+	mot_Masque := '';
+	mot_Adeviner := '';
+	lgchaine := 0;
+	cpt := 1;
+	faute := 0;
+	erreur := TRUE;    //erreur deviendra FAUX si une lettre est trouvée
+
+	Bienvenue;
+	readln;
+
+	mot_Adeviner := saisieMotAdeviner;
+
+	lgchaine := longueur(mot_Adeviner);
+
+	mot_Masque := setMotMasque(lgchaine);
+
+	repeat
+		begin
+		clrscr;
+		writeln ('Joueur 2, veuillez saisir une lettre :');
+		readln (car_Rech);
+		if (Length (car_Rech) = 1) then
+			begin
+			erreur := TRUE;
+			FOR cpt:=1 TO lgchaine DO
+				begin
+				if (Copy (mot_Adeviner, cpt, 1) = car_Rech) then
+					begin
+					mot_Masque := (Copy (mot_Masque, 1, cpt-1)) + (car_Rech) + (Copy (mot_Masque, cpt + 1, lgchaine - cpt));
+					erreur := FALSE
+					end;
+				end;
+				
+			//on regarde s'il y a faute ou pas
+			cptFaute(erreur, faute);
+			infoFaute(faute);
+
+			//représentation du pendu
+			writeln;
+			dessinPendu(faute); 
+			
+			writeln;
+			writeln;	
+			writeln (mot_Masque);
+			readln
+			end
+		else
+			begin
+			writeln ('Erreur !');
+			readln
+			end;
+		end;
+	until (faute = FAUTEMAX) OR (mot_Adeviner = mot_Masque);
+
+	resultat(faute);
+	readln;
+
+END.
